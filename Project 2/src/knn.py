57e8216d4b14b2982754f85b30415960c3ff34d5
Hayden-Perusich <h.perusich2000@outlook.com>
2024-10-01 12:50:13 -0600

    added regression to knn

--- Project 2/src/knn.py
+++ Project 2/src/knn.py
@@ -8,21 +8,42 @@ class KNN:
         self.testing_data = np.array(testing_data)
         self.is_classification = True
     
-    def knn(self, test_point, k, p):
-        distances = []
-        for data_point in self.training_data:
+    def classify(self, testing_data, k, p):
 
-            distance = self.get_distance(test_point[:-1], data_point[:-1], p)
-            distances.append((distance, data_point[-1]))
+        predications = []
+        for test_point in testing_data:
+
+            distances = []
+            for data_point in self.training_data:
+                distance = self.get_distance(test_point[:-1], data_point[:-1], p)
+                distances.append((distance, data_point[-1]))
+            
+            distances = sorted(distances)
+
+            predication = self.vote(distances, k)
+            predications.append(predication)
+
+        return predications
         
-        distances = sorted(distances)
-        
-        #Voting
+    
+    def vote(self, distances, k):
+
+        # Voting / Average
         distances = distances[0:k]
-        classes = [t[1] for t in distances]
-        prediction = Counter(classes).most_common(1)[0][0]
+        if(self.is_classification):
+            classes = [t[1] for t in distances]
+            prediction = Counter(classes).most_common(1)[0][0]
+        else:
+            target_values = [t[1] for t in distances]
+            prediction = sum(target_values)/len(target_values)
 
-        print(prediction)
+        return prediction
+    
+    def get_actual(self):
+        actual = []
+        for row in self.testing_data:
+            actual.append(row[-1])
+        return actual
 
     def get_distance(self, x, y, p):
 
@@ -33,18 +54,19 @@ class KNN:
         return distance
 
 
-
 def main():
 
-    path = "Project 2\data\\raw_data\soybean-small.data"
+    path = "Project 2\data\soybean-small.data"
     data = Data(path, "class")
-
 
     training_set = data.get_training_set(1)
     test_set = data.get_test_set(1)
 
-
     knn = KNN(training_set, test_set)
-    knn.knn(test_set[0], 2, 2)
+    predications = knn.classify(test_set, 2, 2)
+    actual = knn.get_actual()
+
+    print(actual)
+    print(predications)
 
 main()
