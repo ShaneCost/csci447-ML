bba81d6530b9b7771c5d41138462a2726fd0962d
Hayden-Perusich <h.perusich2000@outlook.com>
2024-10-02 15:29:39 -0600

    finial knn??

--- Project 2/src/edited_knn.py
+++ Project 2/src/edited_knn.py
@@ -1,4 +1,5 @@
-from data import Data
+from knn import KNN
+
 
 class EditedKNN(KNN):
 
@@ -6,6 +7,14 @@ class EditedKNN(KNN):
         self.edited_folds = edited_folds
         self.tuning_set = tuning_set
     
+    def edit(self):
+        k = 1
+        p = 2  
+        for point in self.training_data:
+            correct_classification = (self.classify(point, self.training_data, k, p) == self.get_actual(point))
+            if (correct_classification):
+                self.training_data = self.training_data.remove(point)
+
 
 def main():
 
--- Project 2/src/knn.py
+++ Project 2/src/knn.py
@@ -5,67 +5,86 @@ class KNN(object):
     def __init__(self, training_data, testing_data, is_classification = True):
         self.training_data = np.array(training_data)
         self.testing_data = np.array(testing_data)
-        self.is_classification = is_classification
+        # if False assumes regression 
+        self.is_classification = is_classification 
     
-    def classify(self, testing_data, k, p):
+    # k is number of neighbors used in voting or average and p is the p variable in the distance formula
+    def classify(self, test_point, k, p):
 
-        predications = []
-        for test_point in testing_data:
-
-            distances = []
-            for data_point in self.training_data:
-                distance = self.get_distance(test_point[:-1], data_point[:-1], p)
-                distances.append((distance, data_point[-1]))
-            
-            distances = sorted(distances)
-
-            predication = self.vote(distances, k)
-            predications.append(predication)
-
-        return predications
+        # get distance of a point
+        distances = []
+        for data_point in self.training_data:
+            distance = self.get_distance(test_point[:-1], data_point[:-1], p)
+            distances.append((distance, data_point[-1]))
         
+        # containes the list of distances and each associated class
+        distances = sorted(distances)
+        predication = self.vote(distances, k)
+
+        return predication
+    
+    def classify_all(self, k, p):
+
+        predictions = []
+        for test_point in self.testing_data:
+            prediction = self.classify(test_point, k, p)  # Corrected here
+            predictions.append(prediction)
+
+        return predictions
+
+            
     
     def vote(self, distances, k):
 
-        # Voting / Average
+        # voting/average (depending on is_classification)
         distances = distances[0:k]
         if(self.is_classification):
             classes = [t[1] for t in distances]
             prediction = Counter(classes).most_common(1)[0][0]
         else:
+            # impliment the kernal function
             target_values = [t[1] for t in distances]
             prediction = sum(target_values)/len(target_values)
 
+        # prediction for class or target_value
         return prediction
     
-    def get_actual(self):
+    def get_actual(self, point):
+
+        # returns the actual classes/target_values for a point
+        actual = point[-1]
+        return actual
+    
+    def get_actual_all(self):
+
         actual = []
-        for row in self.testing_data:
-            actual.append(row[-1])
+        for point in self.testing_data:
+            actual.append(point[-1])
         return actual
 
     def get_distance(self, x, y, p):
 
+        # Minkowski distance
         x = np.array(x, dtype=np.float64)
         y = np.array(y, dtype=np.float64)   
-        distance = np.sum(np.abs(x - y) ** p) ** (1 / p)
+        distance = np.sum(np.abs(x - y) ** p) ** (1 / p) 
        
         return distance
 
 from data import Data
-# def main():
+def main():
 
-#     path = "Project 2\data\soybean-small.data"
-#     data = Data(path, "class")
+    path = "Project 2\data\soybean-small.data"
+    data = Data(path, "class")
 
-#     training_set = data.get_training_set(1)
-#     test_set = data.get_test_set(1)
+    training_set = data.get_training_set(1)
+    test_set = data.get_test_set(1)
 
-#     knn = KNN(training_set, test_set)
-#     predications = knn.classify(test_set, 2, 2)
-#     actual = knn.get_actual()
+    knn = KNN(training_set, test_set)
+    predications = knn.classify_all(2, 2)
+    actual = knn.get_actual_all()
 
-#     print(actual)
-#     print(predications)
+    print(actual)
+    print(predications)
 
-# main()
+main()
